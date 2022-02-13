17be148d8df323556be768d7bedc24dd71bca6a4
Sam <91824257+heteroyeen@users.noreply.github.com>
2022-02-13 13:04:12 +0800

    Update 13 Feb Ubuntu

--- medium/m15_threeSum.cpp
+++ medium/m15_threeSum.cpp
@@ -10,47 +10,59 @@ Explanation:
 class Solution {
 public:
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-        std::vector<std::vector<int>> ans;
+        std::vector<std::vector<int>> ans = {};
         //sort the vector
         std::sort(nums.begin(),nums.end());
         int vec_end = nums.size();
-
+        if (vec_end < 3) {
+            return {};
+        }
         int anchor = 1;
         int n_left = anchor-1;
         int n_right = anchor+1;
 
-        while (anchor != vec_end-2) {
-            while (n_left >= 0  || n_right < vec_end) {
-                if (-1*anchor == n_left + n_right) {
-                    ans.push_back({anchor, n_left, n_right});
-                    n_left--;n_right++;
+        while (anchor <= vec_end-2) {
+            while (n_left >= 0  && n_right < vec_end) {
+                if (-1*nums[anchor]==nums[n_left]+nums[n_right]) {
+                    ans.push_back({nums[n_left],nums[anchor],nums[n_right]});
+                    break;
                 }
-                else if (-1*anchor > n_left + n_right && n_right < vec_end) {
+                else if (-1*nums[anchor]>nums[n_left]+nums[n_right] && n_right<vec_end) {
                     n_right++;
                 }
-                else if (-1*anchor < n_left + n_right && n_left >= 0) {
+                else if (-1*nums[anchor]<nums[n_left]+nums[n_right] && n_left>0) {
                     n_left--;
                 }
                 // if both iterations has no solution
                 else {break;}
-                
             }
             anchor++;
             n_left = anchor-1;
             n_right = anchor+1;
         }
         // for every possible combination / true answer, we push_back to partial_ans
-        
         return ans;
     }
-
-
 };
 
 int main () {
+    auto timeNow = std::chrono::system_clock::now();
+    time_t timeTick = std::chrono::system_clock::to_time_t(timeNow); 
+    std::string timeMsg = ctime(&timeTick);
+    std::cout << "Exec time = " << timeMsg;
+
+    std::vector<std::vector<int>> ans;
     Solution s1;
-    std::vector<int> testcase={-1,0,1,2,-1,-4};
-    std::cout << "hello world\n" ;
+    std::vector<int> testcase={0,0,0,0};
+    ans = s1.threeSum(testcase);
+    std::cout << "hello world\n";
+    for (auto x : ans) {
+        std::cout << "{";
+        for (auto y : x) {
+            std::cout << y << ", ";
+        }
+        std::cout << "\b\b} \n";
+    }
     
     return 0;
 }
