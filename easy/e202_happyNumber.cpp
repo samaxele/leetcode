5422571e654c6a085efcde643a53efa2f0219eca
Sam <91824257+heteroyeen@users.noreply.github.com>
2022-02-21 01:19:12 +0800

    update 20 Feb Ubuntu

---
+++ easy/e202_happyNumber.cpp
@@ -0,0 +1,52 @@
+#include "CustomHeader/myCustomHeader.h"
+
+/*
+Test if a number is happy, a happy number consist of following:
+start with positive integer. square sum each digits, eg:
+Input 9, 9x9 = 81
+8x8 + 1x1 = 64
+6x6 + 4x4 = 52
+5x5 + 2x2... and so on....
+
+Method 1: Calculate the equation as usual, run 2 pointers: fast and slow
+fast = 2 steps and slow = 1 step
+when (value) fast == slow, we know a close loop occurs
+> we can just return false, need not to find the intersection point
+*/
+
+//trial 1, 4ms(43.11%) / 6.3mb(25.27%)
+class Solution {
+public:
+    bool isHappy(int n) {
+        std::map<int,int> mp = {};
+        auto nextValue = [&](int x) {
+            //enter 81, return 8x8 + 1x1 = 64
+            int ans = 0;
+            while (x >= 1) {
+                ans += (x%10)*(x%10);
+                x /= 10;
+            }
+            return ans;
+        };
+        
+        int ans = nextValue(n);
+        while (ans > 1) {
+            if (mp[ans] > 1) {return false;}
+            mp[ans]++; 
+            ans = nextValue(ans);
+        }
+        return true;
+    }
+};
+
+int main () {
+    Solution s1;
+    int x = 19;
+    if (s1.isHappy(x)) {
+        std::cout << "is true\n";
+    }
+    else
+    std::cout << "is false\n";
+    
+    return 0;
+}
